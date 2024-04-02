4203681139599cad2736735804af95d9623bf938
IshaRode <isharode26@gmail.com>
2024-04-02 15:50:28 +0530

    Added login

---
+++ Server/Schema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+   
+    location: String,
+    name: String,
+    child_safe: Boolean,
+    rating: Number,
+   
+});
+const userModel = mongoose.model("Offbeat-collections", userSchema);
+module.exports = {userModel};
