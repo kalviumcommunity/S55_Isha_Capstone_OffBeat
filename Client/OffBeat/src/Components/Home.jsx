f7a22c35ccca245a5c111d9cad7625999975016a
IshaRode <isharode26@gmail.com>
2024-04-03 14:20:53 +0530

    Added get API

--- Client/OffBeat/src/Components/Home.jsx
+++ Client/OffBeat/src/Components/Home.jsx
@@ -15,6 +15,7 @@ function Home() {
         <h2>Home</h2>
         <Link to="/login"><h2>Login</h2></Link>
         <Link to="/signup"><h2>Signup</h2></Link>
+        
         <h2>About Us</h2>
         <h2>Contact</h2>
     
--- Server/Server.js
+++ Server/Server.js
@@ -4,33 +4,43 @@ require('dotenv').config();
 const port = process.env.PORT;
 const mongoose = require('mongoose')
 
+// Import userModel from Schema.js
+const { userModel } = require('./Schema')
+
 let connectionStatus = "disconnected";
 
 const startDB = async () => {
     try{
-      await  mongoose.connect(process.env.MONGO_URI)
+      await mongoose.connect(process.env.MONGO_URI)
       connectionStatus = "Database connected"
-    }catch(err){
+    } catch(err) {
       console.error("Failed to connect to DB")
       connectionStatus = "error";
     }
-  };
+};
   
-  const stopDB = async () => {
-    await mongoose.disconnect();
-    connectionStatus = "closed"
-  }
-
+const stopDB = async () => {
+  await mongoose.disconnect();
+  connectionStatus = "closed"
+}
 
 app.get('/connect', (req,res) => {
-    res.send(connectionStatus)
+  res.send(connectionStatus)
 })
 
+app.get('/data', async (req, res) => {
+  try {
+    const locData = await userModel.find();
+    res.json(locData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
-    startDB();
-    console.log(`Server running on PORT: ${port}`)
+  startDB();
+  console.log(`Server running on PORT: ${port}`)
 })
 
 module.exports = app;
-
