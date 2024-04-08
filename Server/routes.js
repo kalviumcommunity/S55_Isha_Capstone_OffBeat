109ddaa7ec6b53c6f0b3354dbb7f79144a95f874
IshaRode <isharode26@gmail.com>
2024-04-08 13:41:49 +0530

    Made changes

--- Server/routes.js
+++ Server/routes.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express.Router();
 const { getStatus } = require('./db');
 const { userModel } = require('./Schema');
-require('dotenv').config()
+const Joi = require('joi');
+require('dotenv').config();
 
-app.use(express.json());
+
+const schema = Joi.object({
+    name: Joi.string().required(),
+    location: Joi.string().required(),
+    child_safe: Joi.boolean().requiured(),
+    rating: Joi.number().requiured(),
+    description: Joi.string().requiured(),
+    open_hours: Joi.string().requiured()
+});
 
 app.get('/connect', async (req, res) => {
     const connectionStatus = await getStatus();
@@ -23,13 +32,16 @@ app.get('/data', async (req, res) => {
 
 app.post('/insert', async (req, res) => {
     try {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ error: "Invalid request body" });
+        }
         const insert = await userModel.create(req.body);
         res.json(insert);
-    } catch (err){
+    } catch (err) {
         console.error(err);
-        res.status(500).send('Cannot add');
+        res.status(500).send('Error occoured while inserting data', err);
     }
-})
-
+});
 
 module.exports = app;
