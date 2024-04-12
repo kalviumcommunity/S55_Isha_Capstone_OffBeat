f13fc618eb71c3a91d972335f3796402049d739f
IshaRode <isharode26@gmail.com>
2024-04-12 13:49:27 +0530

    delete req and css

--- Server/routes.js
+++ Server/routes.js
@@ -9,10 +9,10 @@ require('dotenv').config();
 const schema = Joi.object({
     name: Joi.string().required(),
     location: Joi.string().required(),
-    child_safe: Joi.boolean().requiured(),
-    rating: Joi.number().requiured(),
-    description: Joi.string().requiured(),
-    open_hours: Joi.string().requiured()
+    child_safe: Joi.boolean().required(),
+    rating: Joi.number().required(),
+    description: Joi.string().required(),
+    open_hours: Joi.string().required()
 });
 
 app.get('/connect', async (req, res) => {
@@ -31,7 +31,7 @@ app.get('/data', async (req, res) => {
 });
 
 app.post('/insert', async (req, res) => {
-    try {
+    try {  
         const { error } = schema.validate(req.body);
         if (error) {
             return res.status(400).json({ error: "Invalid request body" });
@@ -43,5 +43,41 @@ app.post('/insert', async (req, res) => {
         res.status(500).send('Error occoured while inserting data', err);
     }
 });
+
+app.get('/data/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const getId = await userModel.findById(_id);
+
+        if (!getId) {
+            return res.status(404).json({ error: 'Entity not found' });
+        }
+
+        res.json(getId);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const entityId = req.params.id;
+        const deletedEntity = await userModel.findByIdAndDelete(entityId);
+
+        if (!deletedEntity) {
+            return res.status(404).json({ error: 'Entity not found' });
+        }
+
+        res.json({ message: 'Entity deleted successfully', deletedEntity });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 
 module.exports = app;
