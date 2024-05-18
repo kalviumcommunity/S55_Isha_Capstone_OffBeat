a240aaf9fd75b42243544b983e890162084c6954
IshaRode <isharode26@gmail.com>
2024-05-18 08:11:16 +0530

    endpts

--- Client/OffBeat/src/App.jsx
+++ Client/OffBeat/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Home from './Components/Home'
 import Contact from './Components/Contact'
 import About from './Components/Aboutus'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
     </Routes>
   </BrowserRouter>
   )
