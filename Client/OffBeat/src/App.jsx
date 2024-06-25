542c5f24ddc92d6af6862c6071f84ae804c7edb6
IshaRode <isharode26@gmail.com>
2024-06-25 08:55:52 +0530

    added deployed link

--- Client/OffBeat/src/App.jsx
+++ Client/OffBeat/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Home from './Components/Home'
 import Contact from './Components/Contact'
 import About from './Components/Aboutus'
-import Login from './Components/Login'
+// import Login from './Components/Login'
 import Signup from './Components/Signup'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -13,10 +13,11 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/contact" element={<Contact />} /> 
       <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
+      {/*<Route path="/login" element={<Login />} />*/}
       <Route path="/signup" element={<Signup />} />
+     
 
     </Routes>
   </BrowserRouter>
